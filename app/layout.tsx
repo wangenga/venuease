f372365d90048c22f2483178892cc70e55cf9bea
wangenga <daisyimg65@gmail.com>
2025-09-03 13:01:09 +0300

    speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./Comp/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import ToasterProvider from "./providers/ToasterProvider";
 import LoginModal from "./Comp/Modals/LoginModal";
 import RegisterModal from "./Comp/Modals/RegisterModal";
@@ -36,6 +37,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ToasterProvider />
+        <SpeedInsights />
         {!currentUser && <LoginModal />}
         {!currentUser && <RegisterModal />}
         <Navbar currentUser={currentUser} />
